1db944aa33d160a5449bbe41728d22537757ecba
DeadRigger <zemsksan1997@yandex.ru>
2020-08-28 18:14:53 +0300

    Create block_pay.php

---
+++ views/paysystem/block_pay.php
@@ -0,0 +1,17 @@
+<div class="form-group" style="width: 100%">
+    <div class="col-md-10 my-2">
+        <div class="input-group">
+            <input type="text" id="pay_email" name="email" placeholder="E-MAIL" class="form-control">
+        </div>
+    </div>
+    <div class="col-md-10 my-2">
+        <div class="input-group">
+            <input type="number" id="pay_sum" name="email" placeholder="Сумма пополнения" class="form-control">
+        </div>
+    </div>
+    <div class="col-md-10 my-2">
+        <div class="input-group rounded text-warning">
+            <a id="add_pay" name="email" class="btn btn-default col-md-12"><b>Оплатить</b></a>
+        </div>
+    </div>
+</div>
