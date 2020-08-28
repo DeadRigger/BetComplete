788a6cffe5583f411c48a03efb9a1ec02d597055
DeadRigger <zemsksan1997@yandex.ru>
2020-08-28 18:15:16 +0300

    Create footer.php

---
+++ views/footer.php
@@ -0,0 +1,24 @@
+<div class="container-fluid border-top">
+    <div class="container">    
+        <div class="row">
+            <div class="col-md-9 row text-white align-items-center">
+                <a class="text-white mx-2" href="<? echo $way['url_home']; ?>"><? echo $lang['foot_home']; ?></a>
+                <a class="text-white mx-2" href="<? echo $way['url_term_use']; ?>"><? echo $lang['foot_term_use']; ?></a>
+                <a class="text-white mx-2" href="<? echo $way['url_rule_bet']; ?>"><? echo $lang['foot_rule_bet']; ?></a>
+                <a class="text-white mx-2" href="<? echo $way['url_about']; ?>"><? echo $lang['foot_about']; ?></a>
+                <a class="text-white mx-2" href="<? echo $way['url_feedback']; ?>"><? echo $lang['foot_feedback']; ?></a>
+            </div>
+            <div class="col-md-3 text-white">
+                <span><? echo $lang['foot_support']; ?></span>
+                <span><? echo $lang['foot_support_mail']; ?></span><br>
+                <span><? echo $lang['foot_support_phone']; ?></span>
+            </div>
+        </div>
+        <hr style="background-color: gray" width="80%">
+        <div class="row text-white wrapper">
+            <a href="<? echo $way['webmoney'];?>" target="_blank"><img src="/img/webmoney.png"></a>
+            <a href="<? echo $way['paypal'];?>" target="_blank"><img src="/img/paypal.png"></a>
+            <a href="<? echo $way['YM'];?>" target="_blank"><img src="/img/yamoney.png"></a>
+        </div>
+    </div>
+</div>
